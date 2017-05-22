f420ec40094e6b4b60680d5663328aad545ac0b6
cowlesrk <cowlesrk@yahoo.com>
2017-05-22 15:16:07 -0400

    Created handler for removing items, created a function to store names and clean up code

--- script.js
+++ script.js
@@ -1,32 +1,100 @@
 // Get name and store it to the console.
 
+// const form = document.getElementById("registrar");
+// const input = form.querySelector("input");
+// const ul = document.getElementById("invitedList");
+
+// form.addEventListener("submit", (e) => {
+// 	e.preventDefault();
+
+	// Add name to unordered list below the form
+
+	// const text = input.value;
+
+	// Clear form after name is submitted
+
+	// input.value = "";
+
+	// const li = document.createElement("li");
+	// li.textContent = text;
+
+	// Create RSVP checkbox
+
+	// const label = document.createElement("label");
+	// label.textContent = "Confirmed";
+	// const checkbox = document.createElement("input");
+	// checkbox.type = "checkbox";
+	// label.appendChild(checkbox);
+	// li.appendChild(label);
+
+	// Add a remove button and remove the name
+
+// 	const button = document.createElement("button");
+// 	button.textContent = "remove";
+// 	li.appendChild(button);
+
+// 	ul.appendChild(li);
+// });
+
+
+// Create a handler for the checkbox - when checked, add class of responded to the list item
+
+// ul.addEventListener("change", (e) => {
+// 	const checkbox = event.target;
+// 	const checked = checkbox.checked;
+// 	const listItem = checkbox.parentNode.parentNode;
+
+// 	if (checked) {
+// 		listItem.className = "responded";
+// 	} else {
+// 		listItem.className = "";
+// 	}
+
+// });
+
+// Create a handler for removing items
+
+// ul.addEventListener("click", (e) => {
+// 	if (e.target.tagName === "BUTTON") {
+// 		const li = e.target.parentNode;
+// 		const ul = li.parentNode;
+// 		ul.removeChild(li);
+// 	}
+// });
+
+
+
+//-----------------------------------------------------------------------------
+
+
+
 const form = document.getElementById("registrar");
 const input = form.querySelector("input");
 const ul = document.getElementById("invitedList");
 
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
+// Create a function to get names and store to the console
 
-	// Add name to unordered list below the form
-
-	const text = input.value;
-
-	// Clear form after name is submitted
-
-	input.value = "";
-
+function createLI(text) {
 	const li = document.createElement("li");
 	li.textContent = text;
-
-	// Create RSVP checkbox
-
 	const label = document.createElement("label");
 	label.textContent = "Confirmed";
 	const checkbox = document.createElement("input");
 	checkbox.type = "checkbox";
 	label.appendChild(checkbox);
 	li.appendChild(label);
+	const button = document.createElement("button");
+	button.textContent = "remove";
+	li.appendChild(button);
+	return li;
 
+}
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const text = input.value;
+	input.value = "";
+	const li = createLI(text);
 	ul.appendChild(li);
 });
 
@@ -44,4 +112,14 @@ ul.addEventListener("change", (e) => {
 		listItem.className = "";
 	}
 
+});
+
+// Create a handler for removing items
+
+ul.addEventListener("click", (e) => {
+	if (e.target.tagName === "BUTTON") {
+		const li = e.target.parentNode;
+		const ul = li.parentNode;
+		ul.removeChild(li);
+	}
 });
