9b1b51a9bc403ac7702df13232638e417b7e7511
cowlesrk <cowlesrk@yahoo.com>
2017-05-23 16:54:01 -0400

    Tried to add save functionality but still getting an error

--- script.js
+++ script.js
@@ -8,7 +8,15 @@ const ul = document.getElementById("invitedList");
 
 function createLI(text) {
 	const li = document.createElement("li");
-	li.textContent = text;
+
+	// Create an HTML element so that an editing state can be created
+
+	const span = document.createElement("span");
+	span.textContent = text;
+	li.appendChild(span);
+
+	// Create a checkbox and change an item's label to confirmed
+
 	const label = document.createElement("label");
 	label.textContent = "Confirmed";
 	const checkbox = document.createElement("input");
@@ -56,7 +64,7 @@ ul.addEventListener("change", (e) => {
 
 });
 
-// Create a handler for removing items
+// Create a handler for removing items and editing items
 
 ul.addEventListener("click", (e) => {
 	if (e.target.tagName === "BUTTON") {
@@ -66,6 +74,23 @@ ul.addEventListener("click", (e) => {
 		if (button.textContent === "remove") {
 			ul.removeChild(li);
 	} else if (button.textContent === "edit") {
+		const span = li.firstElementChild;
+		const input = document.createElement("input");
+		input.type = "text";
+		input.value = span.textContent;
+		li.insertBefore(input, span);
+		li.removeChild(span);
+		button.textContent = "save";
+
+		// Move list items to a saved state when the button is clicked
+
+    } else if (button.textContent === "save") {
+		const ipnut = li.firstElementChild;
+		const span = document.createElement("span");
+		span.textContent = input.value;
+		li.insertBefore(span, input);
+		li.removeChild(input);
+		button.textContent = "edit";
     }
   }
 });
